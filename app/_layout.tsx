375385c158984d53d9b7412cf9572f5a5260ede1
Matthew Miss <matt.w.miss@gmail.com>
2024-11-13 16:23:41 -0800

    expo sdk upgraded to version 52. Upgraded many dependencies to latest version to ensure it properly works with expo 52. Commented out Sentry for now as it had some problems with expo sdk 52.
    partially resolves #8

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,55 +11,58 @@ import {EntriesProvider} from "@/context/EntriesContext";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {TagsProvider} from "@/context/TagsContext";
 import NotificationManager from "@/components/notifications/NotificationManager";
+import {UserConfigProvider} from "@/context/UserConfigContext";
 
 NativeWindStyleSheet.setOutput({
     default: "native",
 });
 
-const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+// const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+//
+// Sentry.init({
+//     dsn: 'https://a7fb880fe07737461e4589957315f00d@o4508170779623424.ingest.us.sentry.io/4508170784735232',
+//     debug: true, // Set to `false` in production
+//     // Additional configurations if needed
+//     integrations: [
+//         new Sentry.ReactNativeTracing({
+//             routingInstrumentation,
+//             enableNativeFramesTracking: !isRunningInExpoGo(),
+//         })
+//     ]
+// });
 
-Sentry.init({
-    dsn: 'https://a7fb880fe07737461e4589957315f00d@o4508170779623424.ingest.us.sentry.io/4508170784735232',
-    debug: true, // Set to `false` in production
-    // Additional configurations if needed
-    integrations: [
-        new Sentry.ReactNativeTracing({
-            routingInstrumentation,
-            enableNativeFramesTracking: !isRunningInExpoGo(),
-        })
-    ]
-});
-
-function RootLayout() {
-    const ref = useNavigationContainerRef();
-
-    useEffect(() => {
-        if (ref){
-            routingInstrumentation.registerNavigationContainer(ref);
-        }
-    }, [ref]);
+export default function RootLayout() {
+    // const ref = useNavigationContainerRef();
+    //
+    // useEffect(() => {
+    //     if (ref){
+    //         routingInstrumentation.registerNavigationContainer(ref);
+    //     }
+    // }, [ref]);
 
     const queryClient = new QueryClient();
 
     return (
         <QueryClientProvider client={queryClient}>
-            <ChoresProvider>
-                <EntriesProvider>
-                    <TagsProvider>
-                        <GestureHandlerRootView style={{ flex: 1 }}>
-                            <MenuProvider>
-                                <Stack screenOptions={{headerShown: false}}>
-                                    <Stack.Screen name="init" options={{ headerShown: false }} />
-                                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                                </Stack>
-                            </MenuProvider>
-                            <NotificationManager />
-                        </GestureHandlerRootView>
-                    </TagsProvider>
-                </EntriesProvider>
-            </ChoresProvider>
+            <UserConfigProvider>
+                <ChoresProvider>
+                    <EntriesProvider>
+                        <TagsProvider>
+                            <GestureHandlerRootView style={{ flex: 1 }}>
+                                <MenuProvider>
+                                    <Stack screenOptions={{headerShown: false}}>
+                                        <Stack.Screen name="init" options={{ headerShown: false }} />
+                                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                                    </Stack>
+                                </MenuProvider>
+                                <NotificationManager />
+                            </GestureHandlerRootView>
+                        </TagsProvider>
+                    </EntriesProvider>
+                </ChoresProvider>
+            </UserConfigProvider>
         </QueryClientProvider>
     );
 }
 
-export default Sentry.wrap(RootLayout);
+// export default Sentry.wrap(RootLayout);
